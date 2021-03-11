b613207461b9839b174316c656d45f5305ec922f
Anthony Graignic <anthony@graignic.com>
2021-03-11 23:59:31 +0200

    Add hex lib to generateKeyPair in utils/scripts

--- packages/utils/scripts/generateKeyPair.js
+++ packages/utils/scripts/generateKeyPair.js
@@ -1,7 +1,8 @@
 const { generateKeyPairFromSeed } = require("@stablelib/ed25519");
 const { randomBytes } = require("@stablelib/random");
+const { encode } = require("@stablelib/hex");
 
 const { secretKey, publicKey } = generateKeyPairFromSeed(randomBytes(32));
 
-console.log(secret);
-console.log(public);
+console.log(encode(secretKey));
+console.log(encode(publicKey));
